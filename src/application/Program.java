3c892bae12930b157728f0145001cf668e06c441
Marcos Douglas <marcosdouglas321@gmail.com>
2021-04-01 10:35:32 -0300

    Delet implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +13,7 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
-
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("--- Teste 1: procura pelo id ---");
@@ -42,6 +43,12 @@ public class Program {
 		seller.setName("Martinha Silva");
 		sellerDao.update(seller);
 		System.out.println("Atualizaçao feita!");
+		
+		System.out.println("\n--- Test 6: deletar um vendedor ---");
+		System.out.println("coloque um id para deletar: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("deletado!");
 	}
 
 }
